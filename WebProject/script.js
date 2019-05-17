80f1490ca1e0754628e09637fe71297d67253454
Taya <justataya@gmail.com>
2019-05-17 09:51:32 +0300

    Input validation

--- WebProject/script.js
+++ WebProject/script.js
@@ -2,41 +2,72 @@ let feedbackButton = document.getElementById("feedback-button");
 let fade = document.getElementById("fade");
 let feedbackForm = document.getElementById("feedback-form");
 let submitButton = document.getElementById("submit-button");
+
 feedbackButton.addEventListener("click", showFeedbackForm);
-fade.addEventListener("click",hideFeedbackForm);
+fade.addEventListener("click", hideFeedbackForm);
 submitButton.addEventListener("click", validateFeedbackForm);
 
-window.onload=function () {
+let name = document.getElementById("name");
+name.addEventListener("input", validateName);
+let phone = document.getElementById("phone");
+phone.addEventListener("input", validatePhone);
+let email = document.getElementById("email");
+email.addEventListener("input", validateEmail);
+let message = document.getElementById("message");
+message.addEventListener("input", validateMessage);
+
+window.onload = function () {
     feedbackForm.reset();
 };
 
 function validateFeedbackForm(event) {
-    let name=document.getElementById("name");
-    if(!validateName(name))
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
         alert("Fill form correct!");
-    let phone=document.getElementById("phone");
-    validatePhone(phone);
-    let email=document.getElementById("email");
-    validateEmail(email);
-    let message=document.getElementById("message");
-    validateMessage(message);
+    }
+    else{
     hideFeedbackForm();
+    }
 }
 
-function validateName(name) {
-
+function validateName() {
+    if (name.validity.rangeUnderflow) {
+        name.setCustomValidity("Name has to be >4 characters long.");
+        return false;
+    } else {
+        name.setCustomValidity("");
+        return true;
+    }
 }
 
-function validatePhone(phone) {
-
+function validatePhone() {
+    if (phone.validity.patternMismatch) {
+        phone.setCustomValidity("Phone format: ***-***-****");
+        return false;
+    } else {
+        phone.setCustomValidity("");
+        return true;
+    }
 }
 
-function validateEmail(email) {
-
+function validateEmail() {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("Wrong email");
+        return false;
+    } else {
+        email.setCustomValidity("");
+        return true;
+    }
 }
 
-function validateMessage(message) {
-
+function validateMessage() {
+    if(!message.value){
+        message.setCustomValidity("Empty message");
+        return false;
+    }
+    else{
+        message.setCustomValidity("");
+        return true;
+    }
 }
 
 function showFeedbackForm(event) {
