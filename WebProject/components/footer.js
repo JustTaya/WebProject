34b0aba4dcabd446e48d1b18daa9261862ca7bf0
Taya <justataya@gmail.com>
2019-05-19 00:08:55 +0300

    Progress commit

--- WebProject/components/footer.js
+++ WebProject/components/footer.js
@@ -4,11 +4,11 @@ document.write(`<footer>
         <p class="footer-email">info@name.ru</p>
     </div>
     <ul class="footer-menu">
-        <li class="menu-main underlined">Главная</li>
-        <li class="catalog-main underlined">Каталог</li>
-        <li class="delivery-main underlined">Доставка и оплата</li>
-        <li class="price-main underlined">Прайс-лист</li>
-        <li class="contacts-main underlined">Контакты</li>
+        <li class="main-end underlined">Главная</li>
+        <li class="catalog-end underlined">Каталог</li>
+        <li class="delivery-end underlined">Доставка и оплата</li>
+        <li class="price-end underlined">Прайс-лист</li>
+        <li class="contacts-end underlined">Контакты</li>
     </ul>
     <div class="developer-info">
         <p class="underlined">Разработка сайта —</p>
