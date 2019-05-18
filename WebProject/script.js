762528e524dfc5cf50f62bcd061d32fd647de8dd
Taya <justataya@gmail.com>
2019-05-18 11:07:51 +0300

    Added slider

--- WebProject/script.js
+++ WebProject/script.js
@@ -21,11 +21,10 @@ window.onload = function () {
 };
 
 function validateFeedbackForm(event) {
-    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
         alert("Fill form correct!");
-    }
-    else{
-    hideFeedbackForm();
+    } else {
+        hideFeedbackForm();
     }
 }
 
@@ -60,11 +59,10 @@ function validateEmail() {
 }
 
 function validateMessage() {
-    if(!message.value){
+    if (!message.value) {
         message.setCustomValidity("Empty message");
         return false;
-    }
-    else{
+    } else {
         message.setCustomValidity("");
         return true;
     }
@@ -80,3 +78,50 @@ function hideFeedbackForm(event) {
     feedbackForm.style.display = "none";
     feedbackForm.reset();
 }
+
+let imageNumber = 0;
+
+let images = [
+    document.getElementById('img1'),
+    document.getElementById('img2'),
+    document.getElementById('img3'),
+    document.getElementById('img4'),
+    document.getElementById('img5')
+];
+
+window.onload = async function () {
+    for (; true;) {
+        changeSlide(true);
+        await sleep(4000);
+    }
+};
+
+function changeSlide(isNext) {
+    images[imageNumber].style.opacity = '0';
+
+    if (isNext) {
+        if (imageNumber === 4)
+            imageNumber = 0;
+        else imageNumber++;
+    } else {
+        if (imageNumber === 0)
+            imageNumber = 4;
+        else imageNumber--;
+    }
+
+    images[imageNumber].style.opacity = '1';
+
+    console.log(imageNumber);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.getElementById('prev-slide').onclick = function () {
+    changeSlide(false);
+};
+
+document.getElementById('next-slide').onclick = function () {
+    changeSlide(true);
+};
